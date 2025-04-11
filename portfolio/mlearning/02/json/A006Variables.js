98ea33bc900bc969496e56897e67900974635e78
SteveKim81 <minsubkim81@gmail.com>
2025-04-10 23:55:29 -0400

    4/10 update

---
+++ portfolio/mlearning/02/json/A006Variables.js
@@ -0,0 +1,1 @@
+var globalA006Variables = []
---
+++ portfolio/mlearning/02/json/quizBankPageData.js
@@ -0,0 +1,23 @@
+var globalQuizBankPageData = {
+    'QuizBank': {
+        pageInfo: {
+            groupType: '1',
+            pageType: null,
+            name: 'Question Bank',
+            setting: {},
+            display: true
+        },
+        pages: {
+            'TrashCan': {
+                pageInfo: {
+                    groupType: '1',
+                    pageType: null,
+                    name: 'TrashCan',
+                    setting: {},
+                    display: false
+                },
+                pages: {}
+            }
+        }
+    }
+}
