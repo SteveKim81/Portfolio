d7269e5b858673646cf65d64e9f2c5f0bbcf6ca7
SteveKim81 <minsubkim81@gmail.com>
2025-04-10 23:51:13 -0400

    4/10 updated

---
+++ portfolio/elearning/NRCCRSM/story_content/user.js
@@ -0,0 +1,159 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "60PeP5sZpLj":
+        Script1();
+        break;
+      case "5VRVhgW1vEX":
+        Script2();
+        break;
+      case "6qTekDFAQUJ":
+        Script3();
+        break;
+      case "6WMCLMWZSPN":
+        Script4();
+        break;
+      case "64qvgYLUULU":
+        Script5();
+        break;
+      case "5feI8PjYcDo":
+        Script6();
+        break;
+      case "5WqGeOxXLF8":
+        Script7();
+        break;
+      case "5b8GxlFcSZv":
+        Script8();
+        break;
+      case "6Ccm52QMBNs":
+        Script9();
+        break;
+      case "6HOLPayW1yV":
+        Script10();
+        break;
+      case "6goAeQAKL8I":
+        Script11();
+        break;
+      case "6GjYSEgfudO":
+        Script12();
+        break;
+      case "5rv6WMq3Sgg":
+        Script13();
+        break;
+      case "6XW0IurpJGR":
+        Script14();
+        break;
+      case "5joDNNO4bE4":
+        Script15();
+        break;
+      case "5Xgn11lWV1e":
+        Script16();
+        break;
+      case "6ge8AE0r6XV":
+        Script17();
+        break;
+      case "5oxLP29eahG":
+        Script18();
+        break;
+      case "5yCMZeDQ1lU":
+        Script19();
+        break;
+  }
+}
+
+function Script1()
+{
+  updateTimer(); 
+}
+
+function Script2()
+{
+  updateTimer(); 
+}
+
+function Script3()
+{
+  updateTimer(); 
+}
+
+function Script4()
+{
+  updateTimer(); 
+}
+
+function Script5()
+{
+  updateTimer(); 
+}
+
+function Script6()
+{
+  updateTimer(); 
+}
+
+function Script7()
+{
+  updateTimer(); 
+}
+
+function Script8()
+{
+  updateTimer(); 
+}
+
+function Script9()
+{
+  updateTimer(); 
+}
+
+function Script10()
+{
+  updateTimer(); 
+}
+
+function Script11()
+{
+  updateTimer(); 
+}
+
+function Script12()
+{
+  updateTimer(); 
+}
+
+function Script13()
+{
+  updateTimer(); 
+}
+
+function Script14()
+{
+  updateTimer(); 
+}
+
+function Script15()
+{
+  updateTimer(); 
+}
+
+function Script16()
+{
+  updateTimer(); 
+}
+
+function Script17()
+{
+  stopTimer();
+}
+
+function Script18()
+{
+  updateTimer(); 
+}
+
+function Script19()
+{
+  updateTimer(); 
+}
+
